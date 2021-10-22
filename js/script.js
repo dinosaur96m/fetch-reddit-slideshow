3257b7a466a77afed50e37ec83099e1fd7096bbf
dinosaur96m <nccadmantinaos@gmail.com>
2021-10-22 12:14:11 -0400

    refactoring into smaller chunks of code inside of functions

--- js/script.js
+++ js/script.js
@@ -16,15 +16,12 @@ const requestUrl = "https://www.reddit.com/search.json?q="
 ////////universal functions 
 ///////////////////////////
 
-//hide the image on display, display next image in the queue 
+//cycle through the slideshow
 const takeTheStage = (array) => {
-    //is this fuctnion working and with the desired variables
-    console.log(`tTS is working: tracker = ${picTracker}`)
-    console.log(`need to Display array[${[picTracker]}] with ${array[picTracker]}`)
-    //displya the image found at next url in the array
+    //display the image found at next url in the array
     stage.src = `${array[picTracker]}`
     //increment or reset tracker to 0 to maintain loop
-    if (picTracker === array.length) {
+    if (picTracker === array.length - 1) {
         console.log(`inside tracker checker!: tracker === array.length `)
         picTracker = 0;
     } else if (picTracker >= 0 && picTracker < array.length){
@@ -37,46 +34,16 @@ const takeTheStage = (array) => {
 //filter urls and only return those that are images hosted on reddit
 const siftImages = (url) =>{
     console.log(`filter running on ${url}!`)
-    if  (
-        (url.includes(".jpg") || url.includes(".png")) 
-        // &&
-        // url.includes('i.redd.it')
-    ) {  
-        console.log('url passed!')
+    if  ((url.includes(".jpg") || url.includes(".png")) ) {  
         return true
     } else {
-        console.log('url rejected!')
         return false
     }
-    
 }
 
-/////////////////////////////////
-////Loaded DOM event and function
-/////////////////////////////////
-
-document.addEventListener("DOMContentLoaded", () => {
-    /////////////////////////////////////
-    //submit button listener and function
-    /////////////////////////////////////
-    form.addEventListener("submit", (e) => {
-    //don't let the page refresh!
-    e.preventDefault()
-    
-    //Hide form and text
-    title.style.display = "none"
-    instructions.style.display = "none"
-    form.style.display = "none"
-    //reveal stop button and image
-    stopButton.style.display = "grid"
-    displayContainer.style.display = "block"
-    stage.style.display = "block"
-    //make alt relavent
-    stage.alt = 'getting ' + input.value + ' pics...'
-    ///////////////////
-    //make fetch happen
-    ///////////////////
-    fetch(`${requestUrl}${input.value}%20pictures`)
+//make fetch happen
+const fetchRedditData = () => {
+    fetch(`${requestUrl}${input.value}%20pictures&limit=6`)
     ///when working add+input.value+"%20pictures")
     .then((responseData) => {
         //extract the JSON data from the fetch object
@@ -85,11 +52,12 @@ document.addEventListener("DOMContentLoaded", () => {
     .then((jsonData) => {
         console.log(jsonData.data)
         console.log(jsonData.data.children[0].data.url)
+        const jsonArray = jsonData.data.children
         
         //add images to array
-        for (let i = 0; i < 25; i++) {
+        for (let i = 0; i < jsonArray.length; i++) {
             //grab image url from json
-            const imageUrl = jsonData.data.children[i].data.url
+            const imageUrl = jsonArray[i].data.url
             pics[i] = `${imageUrl}`
             console.log(`captured image${i}: ${imageUrl}`)
         }
@@ -105,6 +73,36 @@ document.addEventListener("DOMContentLoaded", () => {
     .catch((error)=>{
     console.log("ERROR")
     console.log("ERROR")})
+}
+
+const slideshowMode = () => {
+    //hide form and text
+    title.style.display = "none"
+    instructions.style.display = "none"
+    form.style.display = "none"
+    //reveal stop button and image
+    stopButton.style.display = "grid"
+    displayContainer.style.display = "block"
+    stage.style.display = "block"
+    //make alt relavent
+    stage.alt = 'getting ' + input.value + ' pics...'
+
+}
+
+/////////////////////////////////
+////Event Listeners
+/////////////////////////////////
+
+//DOM Loaded Listener
+document.addEventListener("DOMContentLoaded", () => {
+    //submit button listener
+    form.addEventListener("submit", (e) => {
+        //don't let the page refresh!
+        e.preventDefault()
+        //make fetch happen
+        fetchRedditData()
+        //Hide form and text
+        slideshowMode()
     })
     //////////////////////////////////////
     /////Stop Button Listener and Function
